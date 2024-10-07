661117b2383a4de5f043557c770004b344c0129a
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2024-10-07 05:38:33 +0530

    committed

--- Sorting/sortingAlgorithms.java
+++ Sorting/sortingAlgorithms.java
@@ -10,7 +10,9 @@ public class sortingAlgorithms {
         // brickSort(arr);
         // countSort(arr);
         // pigeonholeSort(arr);
-        cycleSort(arr);
+        // cycleSort(arr);
+        // cycleSort0ToN(arr);
+        cyclicSort(arr);
         printArray(arr, "Sorted array");
     }
 
@@ -136,8 +138,6 @@ public class sortingAlgorithms {
 
     }
 
-    // Sorting->
-
     public static void countingSortWithDigit(int arr[], int digit) {
         // find K,
         int n = arr.length;
@@ -248,6 +248,68 @@ public class sortingAlgorithms {
                 swap(arr,index,actualPos);
             }else{
                 index++;
+            }
+        }
+    }
+
+    public static void cycleSort0ToN(int[] arr){
+        int n=arr.length;
+        int index=0;
+        while(index<n){
+            int element = arr[index];
+            int actualPos = element;
+            if(arr[index]<n && arr[index]!=arr[actualPos]){
+                swap(arr,index,actualPos);
+            }else{
+                index++;
+            }
+        }
+    }
+
+    public static void cyclicSort(int arr[]){
+        int n = arr.length;
+        for(int cycles=0;cycles<n-1;cycles++){
+            int pos = cycles;
+            int item = arr[cycles];
+
+            // find number of smaller elements
+            for(int i=cycles+1;i<n;i++){
+                if(arr[i]<item){
+                    pos++;
+                }
+            }
+            // element is at correct position
+            if(pos==cycles){
+                continue;
+            }
+            //ignore duplicates
+            while(arr[pos]==item){
+                pos++;
+            }
+            // if smaller elements are found then perform swapping.
+            if(pos!=cycles){
+                int temp = arr[pos];
+                arr[pos] = item;
+                item = temp;
+            }
+            // check if more swaps are required.
+            while(pos!=cycles){
+                pos = cycles;
+                // find number of smaller elements
+                for(int i=cycles+1;i<n;i++){
+                    if(arr[i]<item){
+                        pos++;
+                    }
+                }
+                //ignore duplicates
+                while(arr[pos]==item){
+                    pos++;
+                }
+                if(item!=arr[pos]){
+                    int temp = arr[pos];
+                    arr[pos] = item;
+                    item = temp;
+                }
             }
         }
     }
