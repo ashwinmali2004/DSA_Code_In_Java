07d03ca67e2bc628e4d63d1a8a1f2f0141c073c2
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2024-10-12 22:04:46 +0530

    committed

---
+++ String/QuestionsOnString.java
@@ -0,0 +1,72 @@
+package String;
+
+public class QuestionsOnString {
+    public static void main(String[] args) {
+        // To print all the substrings of given string
+        printSubString("abc");
+        
+        // To revers the given String
+        printReverseString("abcde");
+        printReverseString2("abcde");
+
+        // To check that the String is Palindrome or not
+        checkPalindrome("naman");
+        checkPalindrome("Ashwin");
+    }
+
+    public static void printSubString(String s){
+        System.out.println("Substrings of the String \""+ s +"\" is :");
+        int size = s.length()*(s.length()+1)/2;
+        int index = 0;
+        String[] str = new String[size];
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length();j++){
+                str[index++] = s.substring(i, j+1);
+            }
+        }
+        for(String a : str){
+            System.out.print(a +",");
+        }
+        System.err.println();
+    }
+
+    public static void printReverseString(String s){
+        System.out.println("Reverse of the String \""+ s +"\" is :");
+        String str = "";
+        for(int i=s.length()-1;i>=0;i--){
+            str+=s.charAt(i);
+        }
+        System.out.println(str);
+    }
+
+    public static void printReverseString2(String s){
+        StringBuilder ans = new StringBuilder(s);
+        int n = ans.length();
+        for(int i=0;i<n/2;i++){
+            char firstChar = ans.charAt(i);
+            char lastChar = ans.charAt(n-1-i);
+            ans.setCharAt(i, lastChar);
+            ans.setCharAt(n-1-i, firstChar);
+        }
+        System.out.println(ans.toString());
+    }
+
+    public static void checkPalindrome(String s){
+        System.out.println("Given String \""+ s +"\" is :");
+        boolean isTrue = true;
+        int start = 0;
+        int end = s.length()-1;
+        while(start<end){
+            if(s.charAt(start) != s.charAt(end)){
+                isTrue = false;
+            }
+            start++;
+            end--;
+        }
+        if (isTrue) {
+            System.out.println(" Palindrome!");
+        }else{
+            System.out.println(" Not a Palindrome!");
+        }
+    }
+}
