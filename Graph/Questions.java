bfa9da47927b4d3991e819911697e3ac07c0b85a
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2025-03-08 19:48:01 +0530

    Questions... of graph

--- Graph/Questions.java
+++ Graph/Questions.java
@@ -4,6 +4,12 @@ import java.util.*;
 
 public class Questions {
     public static void main(String[] args) {
-        
+        // 733 flood fill 
+        // 200 no of islands
+        // 127 word ladder
+        // 126 word ladder 2
+        // 399 Evaluate Division
+        // 1311 get watched videos
+        // 994 rotting oranges
     }
 }
