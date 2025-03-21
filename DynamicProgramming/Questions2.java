53b65713c3b4432f81dbac51b8ee3302183064ae
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2025-03-21 19:33:36 +0530

    Questions on DP

--- DynamicProgramming/Questions2.java
+++ DynamicProgramming/Questions2.java
@@ -78,4 +78,104 @@ public class Questions2 {
         return prev[amount];
     }
     
+    // rod cutting gfg
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return recur(n,m,text1,text2,dp);
+    }
+    int recur(int i, int j, String text1, String text2, int dp[][]){
+        //base case
+        if(i==0 || j==0){
+            dp[i][j] = 0;
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(text1.charAt(i-1) == text2.charAt(j-1)){
+            dp[i][j] = 1+ recur(i-1,j-1,text1,text2,dp);
+            return dp[i][j];
+        }else{
+            int case1 = recur(i-1,j,text1,text2,dp);
+            int case2 = recur(i,j-1,text1,text2,dp);
+            dp[i][j] = Math.max(case1,case2);
+            return dp[i][j];
+        }
+
+    }
+
+    // 1143 longest common subsequence
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n = text1.length();
+        int m = text2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return recur(n,m,text1,text2,dp);
+    }
+    int recur(int i, int j, String text1, String text2, int dp[][]){
+        //base case
+        if(i==0 || j==0){
+            dp[i][j] = 0;
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(text1.charAt(i-1) == text2.charAt(j-1)){
+            dp[i][j] = 1+ recur(i-1,j-1,text1,text2,dp);
+            return dp[i][j];
+        }else{
+            int case1 = recur(i-1,j,text1,text2,dp);
+            int case2 = recur(i,j-1,text1,text2,dp);
+            dp[i][j] = Math.max(case1,case2);
+            return dp[i][j];
+        }
+
+    }
+
+    // 516 longest palindromic common subsequence
+    public int longestPalindromeSubseq(String s) {
+        String text1 = s;
+        StringBuilder str = new StringBuilder(s);
+        String text2 = str.reverse().toString();
+        int n = text1.length();
+        int m = text2.length();
+        int dp[][] = new int[n+1][m+1];
+        for(int i=0;i<n+1;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return recur(n,m,text1,text2,dp);
+    }
+    int recur(int i, int j, String text1, String text2, int dp[][]){
+        //base case
+        if(i==0 || j==0){
+            dp[i][j] = 0;
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+
+        if(text1.charAt(i-1) == text2.charAt(j-1)){
+            dp[i][j] = 1+ recur(i-1,j-1,text1,text2,dp);
+            return dp[i][j];
+        }else{
+            int case1 = recur(i-1,j,text1,text2,dp);
+            int case2 = recur(i,j-1,text1,text2,dp);
+            dp[i][j] = Math.max(case1,case2);
+            return dp[i][j];
+        }
+
+    }
+
+    
 }
