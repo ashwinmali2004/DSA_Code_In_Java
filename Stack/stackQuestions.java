39d833d1f071a176cbb1aa40155e4658861eaa51
Ashwinmali2004 <Ashwinmali2004@gmail.com>
2025-06-27 21:38:18 +0530

    Stack Questions

--- Stack/stackQuestions.java
+++ Stack/stackQuestions.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 public class stackQuestions {
 
+
+    // 20 Leetcode Valid paranthese
+    // count the reversals on gfg
     public static int countRev (String s)
     {
         if(s.length()%2!=0){
@@ -37,6 +40,50 @@ public class stackQuestions {
         return ans;
     }
 
+    // another approch for above to reduce the traversal 
+    public static int countRev2 (String s)
+    {
+        if(s.length()%2!=0){
+            return -1;
+        }
+        
+        int ans = 0, open = 0, close=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='{'){
+                open+=1; 
+            } else {             
+                if(open<=0) {
+                    close++;
+                } else {
+                    open--;
+                }
+            }
+        }
+
+        ans = (int)(Math.ceil(open/2.0) + Math.ceil(close/2.0));
+        return ans;
+    }
+
+    // Q921 Leetcode
+        public int minAddToMakeValid(String s) {
+        int open = 0;
+        int close = 0;
+        for(char c : s.toCharArray()){
+            if(c=='('){
+                open++;
+            }else{
+                if(open>0){
+                    open--;
+                }else{
+                    close++;
+                }
+            }
+        }
+        return open+close;
+    }
+
+    // Q1963 Leetcode
     public static int minSwaps(String s) {
         Stack<Character> stack = new Stack<>();
 
@@ -56,6 +103,26 @@ public class stackQuestions {
         }
         return (count+1)/2;
     }
+
+    // gfg minimumswap for balancing adjacent
+    static int minimumNumberOfSwaps(String s) {
+        int open = 0, close = 0, unbalanced = 0, ans = 0;
+        for(char c : s.toCharArray()){
+            if(c=='['){
+                open++;
+                if(unbalanced>0){
+                    ans += unbalanced;
+                    unbalanced--;
+                }
+            }else{
+                close++;
+                unbalanced = close-open;
+            }
+        }
+        return ans;
+    }
+
+    
     public static void main(String[] args) {
         // to count the number of reversals to take for the balancing the String
         int ans = countRev("}{{}}{{{");
